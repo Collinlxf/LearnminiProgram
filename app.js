8ba915d18a82da71344fd4dcad409a9fb37edb9f
Collinlxf <collinlxf@163.com>
2020-04-14 21:46:42 +0800

    注册时page在做什么

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 App({
 //声明周期函数：后台存活两个小时
   onLaunch: function () {
-    console.log('小程序初始化完成')
+    //console.log('小程序初始化完成')
     //异步调用
     //console.log('小程序初始化完成了：onLaunch')
     //wx.request({
@@ -13,19 +13,13 @@ App({
         //console.log(res)
       //}
     //})
-    setTimeout(function() {
-      const err = new Error()
-
-      throw err
-    }, 3000)
   },
-  onShow: function (options) {
-    console.log('界面显示出来：show')
-  },
-  onHide: function () {
-    console.log('界面被隐藏')
-  },
-  onError: function (msg) {
-    console.log('小程序中发现一些错误')
-  }
+  //onShow: function (options) {
+    //console.log('界面显示出来：show')
+  //},
+  //onHide: function () {
+    //console.log('界面被隐藏')
+  //},
+  //onError: function () {
+  //}
 })
--- page/home/home.js
+++ page/home/home.js
@@ -1,66 +1,39 @@
 // page/home/home.js
+// getApp()获取App（）产生的示例对象
+// const app = getApp()
+
+// const name = app.globalData.name;
+// const age = app.globalData.age;
+
+// 注册一个页面
+// -------------2.页面也有生命周期-------------
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  // 初始化数据
+  data:{
+    message: '哈哈哈'
+  },
+  // -------------1.监听页面的生命周期函数----------------
+  onLoad(){
+    console.log('onLoad')
+  },
+  onReady(){
+    console.log('onReady')
+  },
+  onShow(){
+    console.log('onShow')
+  },
+  onHide(){
+    console.log('onHide')
+  },
+  onUnload(){
+    console.log('onUnload')
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+   // -------------3.监听wxml中的事件----------------
+   handleGetUserInfo(event){
+    console.log(event)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleViewClick(){
+    console.log('被点击了哈哈哈哈')
   }
 })
